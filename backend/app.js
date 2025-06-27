84c5c17ea9192362d3b61c2755b669f3ccbd8e5b
harshranjan2804 <hranjan2804@gmail.com>
2025-06-28 00:59:20 +0530

    fixed CI/CD

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/moods', moodRoutes);
 
-// Start server binding to 0.0.0.0
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running at http://0.0.0.0:${port}`);
-});
 
 
 module.exports = app;
